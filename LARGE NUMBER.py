864cceab10c55148d18abad67aa577cb68d3ec1b
sakthiyokesh <sakthiyokeshs_bcs28@mepcoeng.ac.in>
2026-03-23 10:29:17 +0530

    Add files via upload

---
+++ LARGE NUMBER.py
@@ -0,0 +1,13 @@
+	LARGE NUMBER
+
+a=int(input("Enter the number1:"))
+b=int(input("Enter the number2:"))
+c=int(input("Enter the number3:"))
+if (a>=b):
+   print("large:")
+else if (b>=c):
+   print("large:")
+else if(c>=a):
+   print("large:")
+else:
+   print("Invalid:")
